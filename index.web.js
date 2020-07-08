af48b6b7dc1c7d8d7c0611b34b2bdbdc65c0a63d
TQ <494294315@qq.com>
2020-07-08 14:37:26 +0800

    11

--- index.web.js
+++ index.web.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react';
 
-class Bugly extends Component {
+class Bugly {
     constructor (props) {
-        super (props);
+
     }
 
-    render () {
-        return null;
+    log (msg) {
+        console.log (msg);
+    }
+
+    startWithAppId (buglyAppId,buglyConfig) {
+        this.log ("startWithAppId () is not supported for web");
+    }
+
+    setUserIdentifier (id) {
+        this.log ("setUserIdentifier () is not supported for web");
+    }
+
+    updateAppVersion (ver) {
+        this.log ("updateAppVersion () is not supported for web");
+    }
+
+    setUserValue (k,v) {
+        this.log ("setUserValue () is not supported for web");
+    }
+
+    setTag (tag) {
+        this.log ("setTag () is not supported for web");
+    }
+
+    reportError (etype,error,stack) {
+        this.log ("reportError () is not supported for web");
     }
 }
 
